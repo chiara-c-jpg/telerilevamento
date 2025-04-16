55213a2bdba8ab7139a12311d278c27e222409a2
chiara-c-jpg <chiara.tattini@studio.unibo.it>
2025-04-16 17:36:53 +0200

    Create 11_function.R

---
+++ 11_function.R
@@ -0,0 +1,27 @@
+# R code for generating new function
+
+somma<-function(x,y) { 
+ z=x+y
+ return(z) 
+ }  
+differenza<-function
+
+pannello<-function(x,y) { 
+  par(mfrow=c(x,y))
+   }
+positivo<-function(x) {
+   }
+if(x>o) {
+  print("Questo numero è positivo, non lo say")
+      }
+else if(x<o) {
+  print("questo numero è negativo,studia!")
+   }
+else if(x=o)  {
+  print( "Lo zero nonè nè positivo  nè negativo, da sempre!")
+   }
+  flipint<-function(x)  {
+  x=flip(x)
+  plot(x)
+}
+sdpca=flipint(sdpca)
